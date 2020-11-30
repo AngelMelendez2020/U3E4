916cd5e6faa67256edea38d5f6e4249a13aaae59
AngelMelendez2020 <71795575+AngelMelendez2020@users.noreply.github.com>
2020-11-29 22:21:52 -0600

    Create Nodo.java

---
+++ Tarea2pila/Nodo.java
@@ -0,0 +1,12 @@
+package Tarea2pila;
+
+
+public class Nodo {
+  int num,p;
+    Nodo siguiente;
+    public Nodo(int num,int p){
+        this.num=num;
+        this.p=p;
+        this.siguiente=null;
+    }
+}
